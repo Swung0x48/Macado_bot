ff4d3fb5109ac20cdb70dd6291bf0fe181717bd7
Swung0x48 <swung0x48@outlook.com>
2020-04-18 23:56:27 +0800

    Ditched MessageEventArgs. Using Telegram.Bot.Types.Update instead.

--- Bot.cs
+++ Bot.cs
@@ -2,11 +2,12 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Macado_bot.IO;
-using Macado_bot.Utils;
 using Macado_bot.Misc;
-using Newtonsoft.Json;
+using Macado_bot.Utils.Commands;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace Macado_bot
 {
@@ -14,13 +15,17 @@ namespace Macado_bot
     {
         public static TelegramBotClient BotClient;
 
-        public static async Task Init(string apiKey)
+        public static async Task Init(string apiKey, CancellationTokenSource cts)
         {
             BotClient = new TelegramBotClient(apiKey); // Get the API Key to start bot.
             var botInstance = BotClient.GetMeAsync().Result; // Get the bot instance info.
             Console.WriteLine($"ID: {botInstance.Id} \nName: {botInstance.FirstName}."); // Survival confirmation.
-            BotClient.OnMessage += OnMessage;                                        // Add OnMessage() to bot Client's OnMessage listener
-            BotClient.StartReceiving();
+            BotClient.OnUpdate += OnUpdate;                                        // Add OnMessage() to bot Client's OnMessage listener
+            // TODO : delete above
+            BotClient.StartReceiving(
+                new[] { UpdateType.Message },
+                    cts.Token
+                    );
 
             while (true)                                                                    
             {
@@ -31,36 +36,64 @@ namespace Macado_bot
 
         }
 
-        static async void OnMessage(object sender, MessageEventArgs e) 
+        //static async void OnMessage(object sender, MessageEventArgs e) // TODO : delete this
+        static async void OnUpdate(object sender, UpdateEventArgs e) 
         {
             var botInstance = BotClient.GetMeAsync().Result; // Get the bot instance info.
-            if (e.Message.Text != null)
-            {
-                
-                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.\n");
-                if (e.Message.Text == "/uptime")                                            // TODO : Implement CmdProcessor to replace if blocks
-                {
-                    await BotClient.SendTextMessageAsync(e.Message.Chat, $"Current uptime is {Vars.Uptime}");
-                }
-                else if (e.Message.Text == "/start")
-                {
-                    await BotClient.SendTextMessageAsync(e.Message.Chat, " ");
-                }
-                else if (e.Message.Text == "/settings")
-                {
-                    
-                }
-                else if (e.Message.Text == "/info" || e.Message.Text == $"/info@{botInstance.Username}")
-                {
-                    await Networking.GetUpInfo(e.Message.Chat);
+            var update = e.Update;
+            if (update.Type != UpdateType.Message) return;
+            if (update.Message.From.IsBot) return;
+            // if (update.Message.Chat.Type != ChatType.Private) return;
 
-                }
-                else if (e.Message.Text == "/getlatest" || e.Message.Text == $"/getlatest@{botInstance.Username}")
-                {
-                    await Networking.GetLatestVidInfo(e.Message.Chat);
-                }
-                
+            if (update.Message.From.Id == Vars.CurrentConf.OwnerUID)
+            {
+                await OwnerLogic(update).ConfigureAwait(false);
             }
+            else
+            {
+                await UserLogic(update).ConfigureAwait(false);
+            }
+            
+            
+            
+            // if (e.Message.Text != null)    // TODO : delete this
+            // {
+            //     
+            //     Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.\n");
+            //     if (e.Message.Text == "/uptime")                                            // TODO : Implement CmdProcessor to replace if blocks
+            //     {
+            //         await BotClient.SendTextMessageAsync(e.Message.Chat, $"Current uptime is {Vars.Uptime}");
+            //     }
+            //     else if (e.Message.Text == "/start")
+            //     {
+            //         await BotClient.SendTextMessageAsync(e.Message.Chat, " ");
+            //     }
+            //     else if (e.Message.Text == "/settings")
+            //     {
+            //         
+            //     }
+            //     else if (e.Message.Text == "/info" || e.Message.Text == $"/info@{botInstance.Username}")
+            //     {
+            //         await Networking.GetUpInfo(e.Message.Chat);
+            //
+            //     }
+            //     else if (e.Message.Text == "/getlatest" || e.Message.Text == $"/getlatest@{botInstance.Username}")
+            //     {
+            //         await Networking.GetLatestVidInfo(e.Message.Chat);
+            //     }
+            //     
+            // }
+        }
+
+        private static async Task UserLogic(Update update)
+        {
+            if (await CommandInstance.CommandManager.Execute(Bot.BotClient, update))
+                return;
+        }
+
+        private static async Task OwnerLogic(Update update)
+        {
+            throw new NotImplementedException();            // TODO : Implement this.
         }
     }
 }
